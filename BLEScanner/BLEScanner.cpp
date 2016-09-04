456f1e3d4478224a6cee4b5708e31e5508a38f5c
Uri Shaked <uri@salsa4fun.co.il>
2016-09-04 19:15:43 +0300

    Implement BLE Watcher
    We now scan for all devices with the services that we need, and then pair with the first matching device.

--- BLEScanner/BLEScanner.cpp
+++ BLEScanner/BLEScanner.cpp
@@ -11,11 +11,63 @@
 #include <wrl/event.h>
 #include <collection.h>
 #include <ppltasks.h>
+#include <string>
+#include <sstream> 
+#include <iomanip>
 
 using namespace Platform;
 using namespace Windows::Devices;
 
-#define TARGET_DEVICE_NAME L"LEDBLE-5BF8CCEF" 
+Enumeration::DeviceWatcher^ w;
+
+std::wstring formatBluetoothAddress(unsigned long long BluetoothAddress) {
+	std::wostringstream ret;
+	ret << std::hex << std::setfill(L'0')
+		<< std::setw(2) << ((BluetoothAddress >> (5 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (4 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (3 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (2 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (1 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (0 * 8)) & 0xff);
+	return ret.str();
+}
+
+void connectToDevice(String^ address) {
+	String^ queryString = L"(System.Devices.Aep.DeviceAddress:=\"" + address + "\")";
+	Windows::Foundation::Collections::IVector<String^>^ requestedProperties = ref new Collections::Vector<String^>();
+	requestedProperties->Append(L"System.Devices.Aep.DeviceAddress");
+	requestedProperties->Append(L"System.Devices.Aep.IsConnected");
+	auto deviceWatcher = Enumeration::DeviceInformation::CreateWatcher(queryString, requestedProperties, Enumeration::DeviceInformationKind::AssociationEndpoint);
+
+	deviceWatcher->Added += ref new Windows::Foundation::TypedEventHandler<Enumeration::DeviceWatcher ^, Enumeration::DeviceInformation ^>(
+		[](Enumeration::DeviceWatcher ^watcher, Enumeration::DeviceInformation ^deviceInfo)->void {
+		std::wcout << L"BLE Device Information:" << std::endl;
+		std::wcout << L"-> Name: " << deviceInfo->Name->Data() << std::endl;
+		std::wcout << L"-> Paired? " << (deviceInfo->Pairing->IsPaired ? L"YES" : L"NO") << std::endl;
+		if (!deviceInfo->Pairing->IsPaired) {
+			auto customPairing = deviceInfo->Pairing->Custom;
+			customPairing->PairingRequested += ref new Windows::Foundation::TypedEventHandler<Windows::Devices::Enumeration::DeviceInformationCustomPairing ^, Windows::Devices::Enumeration::DevicePairingRequestedEventArgs ^>(
+				[](Enumeration::DeviceInformationCustomPairing ^pairing, Enumeration::DevicePairingRequestedEventArgs ^args) {
+				std::wcout << "Device pairing request: " << args->PairingKind.ToString()->Data() << std::endl;
+				args->Accept();
+			});
+
+			std::wcout << "*** Pairing with the device..." << std::endl;
+			auto task = concurrency::create_task(customPairing->PairAsync(Enumeration::DevicePairingKinds::None | Enumeration::DevicePairingKinds::ConfirmOnly));
+			task.then([](Enumeration::DevicePairingResult^ result)->void {
+				std::wcout << "Device pairing result: " << result->Status.ToString()->Data() << std::endl;
+			});
+		}
+	});
+
+	deviceWatcher->Updated += ref new Windows::Foundation::TypedEventHandler<Windows::Devices::Enumeration::DeviceWatcher ^, Windows::Devices::Enumeration::DeviceInformationUpdate ^>(
+		[](Enumeration::DeviceWatcher ^watcher, Enumeration::DeviceInformationUpdate ^deviceInfoUpdate)->void {
+		// Nada
+	});
+
+	deviceWatcher->Start();
+	w = deviceWatcher;
+}
 
 int main(Array<String^>^ args) {
 	Microsoft::WRL::Wrappers::RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
@@ -24,54 +76,24 @@ int main(Array<String^>^ args) {
 	auto characteristicUUID = Bluetooth::GenericAttributeProfile::GattCharacteristic::ConvertShortIdToUuid(0xffe9);
 	Bluetooth::GenericAttributeProfile::GattDeviceService^ deviceService;
 
-	String^ queryString = L"(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")";
-	Windows::Foundation::Collections::IVector<String^>^ requestedProperties = ref new Collections::Vector<String^>();
-	requestedProperties->Append(L"System.Devices.Aep.DeviceAddress");
-	requestedProperties->Append(L"System.Devices.Aep.IsConnected");
-	auto deviceWatcher = Enumeration::DeviceInformation::CreateWatcher(queryString, requestedProperties, Enumeration::DeviceInformationKind::AssociationEndpoint);
+	Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher^ bleAdvertisementWatcher = ref new Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher();
+	bleAdvertisementWatcher->ScanningMode = Bluetooth::Advertisement::BluetoothLEScanningMode::Active;
+	bleAdvertisementWatcher->Received += ref new Windows::Foundation::TypedEventHandler<Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher ^, Windows::Devices::Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs ^>(
+		[serviceUUID, bleAdvertisementWatcher](Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher ^watcher, Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs^ eventArgs) {
+		auto serviceUuids = eventArgs->Advertisement->ServiceUuids;
+		unsigned int index = -1;
+		if (serviceUuids->IndexOf(serviceUUID, &index)) {
+			String^ strAddress = ref new String(formatBluetoothAddress(eventArgs->BluetoothAddress).c_str());
+			std::wcout << "Target service found on device: " << strAddress->Data() << std::endl;
 
-	deviceWatcher->Added += ref new Windows::Foundation::TypedEventHandler<Enumeration::DeviceWatcher ^, Enumeration::DeviceInformation ^>(
-		[](Enumeration::DeviceWatcher ^watcher, Enumeration::DeviceInformation ^deviceInfo)->void {
-		std::wcout << L"Found some BLE device" << std::endl;
-		String^ localName = deviceInfo->Name;
-		String^ prop = safe_cast<String^>(deviceInfo->Properties->Lookup(L"System.Devices.Aep.DeviceAddress"));
-		Boolean connected = safe_cast<Boolean>(deviceInfo->Properties->Lookup(L"System.Devices.Aep.IsConnected"));
+			connectToDevice(strAddress);
 
-		if (localName) {
-			std::wcout << L"Device name:" << deviceInfo->Name->Data() << std::endl;
-			std::wcout << L"Adress:" << prop->Data() << std::endl;
-			std::wcout << L"Paired? " << (deviceInfo->Pairing->IsPaired ? L"YES" : L"NO") << std::endl;
-
-			std::wcout << L"Connected? " << (connected ? L"YES" : L"NO") << std::endl;
-
-			if (localName->Equals(TARGET_DEVICE_NAME)) {
-				auto customPairing = deviceInfo->Pairing->Custom;
-				customPairing->PairingRequested += ref new Windows::Foundation::TypedEventHandler<Windows::Devices::Enumeration::DeviceInformationCustomPairing ^, Windows::Devices::Enumeration::DevicePairingRequestedEventArgs ^>(
-					[](Enumeration::DeviceInformationCustomPairing ^pairing, Enumeration::DevicePairingRequestedEventArgs ^args) {
-					std::wcout << "Device pairing request: " << args->PairingKind.ToString()->Data() << std::endl;
-					args->Accept();
-				});
-
-				std::wcout << "*** Pairing with the device..." << std::endl;
-				auto task = concurrency::create_task(customPairing->PairAsync(Enumeration::DevicePairingKinds::None | Enumeration::DevicePairingKinds::ConfirmOnly));
-				task.then([](Enumeration::DevicePairingResult^ result)->void {
-					std::wcout << "Device pairing result: " << result->Status.ToString()->Data() << std::endl;
-				});
-			}
-
-			std::wcout << L"---" << std::endl;
+			bleAdvertisementWatcher->Stop();
 		}
-	}
-	);
-
-	deviceWatcher->Updated += ref new Windows::Foundation::TypedEventHandler<Windows::Devices::Enumeration::DeviceWatcher ^, Windows::Devices::Enumeration::DeviceInformationUpdate ^>(
-		[](Enumeration::DeviceWatcher ^watcher, Enumeration::DeviceInformationUpdate ^deviceInfoUpdate)->void {
-		std::wcout << "Device Watcher: Updated" << std::endl;
 	});
+	bleAdvertisementWatcher->Start();
 
-	deviceWatcher->Start();
-
-
+	// Look for paired devices with the relevant service GUID, and once found start controlling them
 	String^ selector = Bluetooth::GenericAttributeProfile::GattDeviceService::GetDeviceSelectorFromUuid(serviceUUID);
 	auto gattServiceWatcher = Enumeration::DeviceInformation::CreateWatcher(selector);
 
